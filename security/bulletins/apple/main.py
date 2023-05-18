2c3895b49d63394455d2c806ac9f275f18abe732
Arsenii Kostromin <kostromin.arsenii@ya.ru>
2023-05-18 23:29:47 +0300

    Remove telegram bot req

--- security/bulletins/apple/main.py
+++ security/bulletins/apple/main.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import pathlib
 import typing
@@ -6,7 +5,6 @@ import typing
 import parsel
 import requests
 import sentry_sdk
-import telegram
 
 
 def get_security_updates() -> typing.Iterator:
@@ -23,7 +21,7 @@ def get_security_updates() -> typing.Iterator:
         yield url.split("/")[-1], name
 
 
-async def main():
+def main():
     sentry_token = os.environ["TOKEN_SENTRY"]
     sentry_sdk.init(
         dsn=sentry_token,
@@ -32,15 +30,22 @@ async def main():
     path = pathlib.Path(os.environ["DATA_PATH"])
     new_data = dict(get_security_updates())
     delta = new_data.keys() - path.read_text().splitlines()
-    bot = telegram.Bot(os.environ["TOKEN_TELEGRAM"])
 
     chat_id = os.environ["CHAT_ID"]
     for key in delta:
         text = f"{new_data[key]}\nhttps://support.apple.com/kb/{key}\n"
-        await bot.send_message(text=text, chat_id=chat_id)
+        requests.get(
+            "https://api.telegram.org/bot"
+            + os.environ["TOKEN_TELEGRAM"]
+            + "/sendMessage"
+            + "?chat_id="
+            + chat_id
+            + "&text="
+            + text
+        )
     with open(path, "a") as f:
         f.writelines(sorted([key + "\n" for key in delta]))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
